a711586bba23c56a2f55af7811989b2f81e61028
fabriguaglia <fabriguaglia72@gmail.com>
2023-10-23 08:13:18 -0300

    config changes

--- src/Layout.js
+++ src/Layout.js
@@ -6,7 +6,9 @@ export default function Layout() {
     <>
       <div className="bg-dark">
         <div className="container d-flex justify-content-center">
-          <img className="" src={ElevateHead} height={100} alt="" />
+        <a href="https://www.santa-ana.edu.ar/" target="_blank" rel="noreferrer">
+            <img src={ElevateHead} height={100} alt="" />
+          </a>
         </div>
         <section
           className="container fliphead p-2 d-flex flex-column justify-content-center align-items-center mt-1"
