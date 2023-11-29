e8776a3a1ae35d9d4d0fe028e14015fac55ae2e3
fabriguaglia <fabriguaglia72@gmail.com>
2023-11-29 09:12:36 -0300

    final model

--- src/FlipBook.js
+++ src/FlipBook.js
@@ -20,20 +20,6 @@ import Image14 from "./assets/images/NewsLetter/14.png";
 import Image15 from "./assets/images/NewsLetter/15.png";
 import Image16 from "./assets/images/NewsLetter/16.png";
 import Image17 from "./assets/images/NewsLetter/17.png";
-import Image18 from "./assets/images/NewsLetter/18.png";
-import Image19 from "./assets/images/NewsLetter/19.png";
-import Image20 from "./assets/images/NewsLetter/20.png";
-import Image21 from "./assets/images/NewsLetter/21.png";
-import Image22 from "./assets/images/NewsLetter/22.png";
-import Image23 from "./assets/images/NewsLetter/23.png";
-import Image24 from "./assets/images/NewsLetter/24.png";
-import Image25 from "./assets/images/NewsLetter/25.png";
-import Image26 from "./assets/images/NewsLetter/26.png";
-import Image27 from "./assets/images/NewsLetter/27.png";
-import Image28 from "./assets/images/NewsLetter/28.png";
-import Image29 from "./assets/images/NewsLetter/29.png";
-import Image30 from "./assets/images/NewsLetter/30.png";
-import Image31 from "./assets/images/NewsLetter/31.png";
 
 export default class FlipBook extends Component {
   render() {
@@ -93,48 +79,6 @@ export default class FlipBook extends Component {
           </div>
           <div className="">
             <img src={Image17} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image18} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image19} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image20} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image21} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image22} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image23} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image24} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image25} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image26} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image27} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image28} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image29} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image30} width="422" height="601" alt="" />
-          </div>
-          <div className="">
-            <img src={Image31} width="422" height="601" alt="" />
           </div>
         </HTMLFlipBook>
       </>
